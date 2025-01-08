4601d3c8e5c738be5aa3e586e68da4d8492d7d2a
De Stout <destoutdev@gmail.com>
2025-01-07 19:54:49 -0500

    Adjust crystal lights and materials. Add noise projection to crystal lights. Add golden crystal material.

--- Maps/BotSim/Temple/Assets/Props/Monuments/Crystal.gd
+++ Maps/BotSim/Temple/Assets/Props/Monuments/Crystal.gd
@@ -6,6 +6,7 @@ extends Node3D
 @export var osc_height := 0.5
 @export var osc_time := 8.0
 @export var rotate_time := 20.0
+@export var rotate_dir := true
 
 var time := randf() * TAU
 
@@ -17,6 +18,8 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var osc_offset = (osc_height / 2.0) * sin((time * TAU) / osc_time)
 	crystal.global_position.y = crystal_start_pos.y + osc_offset
-	crystal.rotate_y((TAU / rotate_time) * delta)
+	
+	var dir = (int(rotate_dir) * 2) - 1
+	crystal.rotate_y(dir * (TAU / rotate_time) * delta)
 	
 	time += delta
